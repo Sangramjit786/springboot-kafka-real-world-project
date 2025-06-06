2ea45e01c11e6620260c454b4974166a136964cd
Rohit <nathrohit48@gmail.com>
2025-06-06 10:35:01 +0530

    Wikimedia Producer Spring Boot Project Setup and Configure Wikimedia Producer and Create a Topic and Wikimedia Producer Implementation

---
+++ src/test/java/net/javaguides/springboo/SpringbootKafkaRealWorldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaRealWorldProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
