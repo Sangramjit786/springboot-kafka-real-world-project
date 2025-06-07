9eac39b659f7aeda5ad552ea7d3918245574774f
Rohit <nathrohit48@gmail.com>
2025-06-07 08:27:18 +0530

    Save Wikimedia Data into MySQL Database.

--- kafka-consumer-database/src/main/java/net/javaguides/springboot/KafkaDatabaseConsumer.java
+++ kafka-consumer-database/src/main/java/net/javaguides/springboot/KafkaDatabaseConsumer.java
@@ -1,5 +1,7 @@
 package net.javaguides.springboot;
 
+import net.javaguides.springboot.entity.WikimediaData;
+import net.javaguides.springboot.repository.WikimediaDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,6 +12,12 @@ public class KafkaDatabaseConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
 
+    private WikimediaDataRepository dataRepository;
+
+    public KafkaDatabaseConsumer(WikimediaDataRepository dataRepository) {
+        this.dataRepository = dataRepository;
+    }
+
 
     @KafkaListener(
             topics = "wikimedia_recentchange",
@@ -19,9 +27,9 @@ public class KafkaDatabaseConsumer {
 
         LOGGER.info(String.format("Event message received -> %s", eventMessage));
 
-//        WikimediaData wikimediaData = new WikimediaData();
-//        wikimediaData.setWikiEventData(eventMessage);
-//
-//        dataRepository.save(wikimediaData);
+        WikimediaData wikimediaData = new WikimediaData();
+        wikimediaData.setWikiEventData(eventMessage);
+
+        dataRepository.save(wikimediaData);
     }
 }
